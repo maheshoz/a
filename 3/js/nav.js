94f3da2919fc76123ccf4893c5ec65ba43cb755c
maheshoz <35638589+maheshoz@users.noreply.github.com>
2018-07-21 01:00:20 +0530

    Add files via upload

---
+++ 3/js/nav.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#open').on('click',function(){
+        $('#overlay').removeClass('close');
+    }); 
+
+    $('#close').on('click',function(){
+        $('#overlay').addClass('close');
+        
+    });
+});
+
